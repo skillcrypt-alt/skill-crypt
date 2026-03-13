b31a6d93d0b00805642f069cb11e001e75b233e4
skillcrypt-alt <skillcrypt-alt@users.noreply.github.com>
2026-03-12 21:53:05 -0500

    fresh oracle and group

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
  */
 
 export const DEFAULTS = {
-  oracleAddress: '0x4462d91640a2c6fb884355e7c2937e25d9f2078f',
-  groupId: 'e7fa4d5d288b282fa8a961b5bba61fab',
+  oracleAddress: '0x51f05678e274862114fab0b10e387f23f021f50c',
+  groupId: '502618c312cbf389b514092586f08f58',
   env: 'production'
 };
