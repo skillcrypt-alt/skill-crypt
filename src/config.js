c0bfeec173eb5be6c35d5f6ee8d865405b9f775f
skillcrypt-alt <skillcrypt-alt@users.noreply.github.com>
2026-03-14 18:09:25 -0500

    config: new oracle + group (demo6)

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
  */
 
 export const DEFAULTS = {
-  oracleAddress: '0x3226cBb76f551218e008508F526cD9A052531339',
-  groupId: '33088d15f2a9bbf6ef712763b8a37e53',
+  oracleAddress: '0x50159D2EC39C93d9140AceD64650b39507F298DE',
+  groupId: '72fc0ad44e6d30d7120f46c3f53b9b43',
   env: 'production'
 };
